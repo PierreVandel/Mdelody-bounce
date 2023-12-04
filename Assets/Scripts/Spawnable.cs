40969a215725c4c0b335a8a0a817fb6e7affbc6f
WillN-Git <wfndefo@gmail.com>
2023-12-04 11:03:09 +0100

    Spawnable script ready for testing

--- Assets/Scripts/Spawnable.cs
+++ Assets/Scripts/Spawnable.cs
@@ -6,32 +6,62 @@ public class Spawnable : MonoBehaviour
 {
     public GameObject spawnObject;
 
+    private GameObject[] Balls;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Spawn onclick
+        SpawnOnClick();
+    }
+
+    public void SpawnOnClick()
+    {
         if (Input.GetMouseButtonDown(0))
         {
+            UpdateBalls();
             SpawnObject();
         }
     }
 
-    void SpawnObject()
+    private void SpawnObject()
     {
-        Vector3 pos = GetMousePosition();
+        Vector3 mousePosition = GetMousePosition();
+        
+        if (DetectBallOverlap(new Vector2(mousePosition.x, mousePosition.y)))
+            return;
+
         Vector3 offset = new Vector3(0, 0, 10);
 
-        Instantiate(spawnObject, pos + offset, Quaternion.identity);
+        Instantiate(spawnObject, mousePosition + offset, Quaternion.identity);
     }
 
     private Vector3 GetMousePosition()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void UpdateBalls()
+    {
+        Balls = GameObject.FindGameObjectsWithTag("Ball");
+    }
+
+    private bool DetectBallOverlap(Vector2 Pos)
+    {
+        foreach (GameObject ball in Balls)
+        {
+            Collider2D c = ball.GetComponent<CircleCollider2D>();
+            if (c.OverlapPoint(Pos))
+            {
+                Debug.Log("Ball hit");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
