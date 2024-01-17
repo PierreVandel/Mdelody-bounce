753b468752f0a62a5cd0bf19a42d71af106ac62c
WillN-Git <wfndefo@gmail.com>
2024-01-17 09:56:41 +0100

    Update : BallID script

--- Assets/Scripts/BallID.cs
+++ Assets/Scripts/BallID.cs
@@ -9,11 +9,34 @@ public class BallID : MonoBehaviour
     
 
     // Start is called before the first frame update
-    //void Start()
-    //{
-    //    ID = BallCount;
-    //    BallCount++;
-    //}
+    void Start()
+    {
+        bool a = Input.GetKey(KeyCode.A);
+        bool z = Input.GetKey(KeyCode.Z);
+        bool e = Input.GetKey(KeyCode.E);
+        bool r = Input.GetKey(KeyCode.R);
+        bool t = Input.GetKey(KeyCode.T);
+        bool y = Input.GetKey(KeyCode.Y);
+        bool u = Input.GetKey(KeyCode.U);
+
+
+        if (a)
+            ID = 7 * BallCount++ + 0;
+        else if (z)
+            ID = 7 * BallCount++ + 1;
+        else if (e)
+            ID = 7 * BallCount++ + 2;
+        else if (r)
+            ID = 7 * BallCount++ + 3;
+        else if (t)
+            ID = 7 * BallCount++ + 4;
+        else if (y)
+            ID = 7 * BallCount++ + 5;
+        else if (u)
+            ID = 7 * BallCount++ + 6;
+        else
+            ID = BallCount++;
+    }
 
     // Update is called once per frame
     void Update()
